66ea36eed0587f51362fdbeadb6d5243a042474c
Jéssica <jessicah.dias23@hotmail.com>
2023-02-25 01:46:28 -0300

    Criado componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { LoginComponent } from './views/login/login.component';
+import { UsuarioComponent } from './views/usuario/usuario.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    InputTextModule,
+    ButtonModule,
     CardModule
   ],
   providers: [],
